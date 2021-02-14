97a01ddddf9e8f52dfc3f18b6d2600606d2e6c61
et-sollertis-animi <hiaarushgupta@gmail.com>
2021-02-14 15:29:06 -0800

    remove script for ctrl+u prevention

--- scripts/script.js
+++
@@ -1,9 +0,0 @@
-document.onkeydown = function (e) {
-    if (e.ctrlKey &&
-        (e.keyCode > 1)) {
-        alert('not allowed');
-        return false;
-    } else {
-        return true;
-    }
-};
